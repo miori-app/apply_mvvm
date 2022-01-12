5d037d3a5148e1b90a0477f749c7ed57d4c000d5
iammiori <immioriii@gmail.com>
2022-01-13 01:55:29 +0900

    [feat] device 너비, 높이 상수 구현

---
+++ LikeBlog/LikeBlog/Configuration/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  LikeBlog
+//
+//  Created by miori Lee on 2022/01/12.
+//
+
+import Foundation
+import UIKit
+
+struct Constant {
+    static let deviceWidth = UIScreen.main.bounds.width
+    static let deviceHeight = UIScreen.main.bounds.height
+}
