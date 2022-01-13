0964f2870b3d154b4679b558777d2bf0c08d2e82
iammiori <immioriii@gmail.com>
2022-01-14 01:11:19 +0900

    [modify] filterview sortBtn 사이즈 수정

--- LikeBlog/LikeBlog/View/FilterView.swift
+++ LikeBlog/LikeBlog/View/FilterView.swift
@@ -51,7 +51,7 @@ class FilterView : UITableViewHeaderFooterView {
         sortBtn.snp.makeConstraints {
             $0.top.equalToSuperview()
             $0.trailing.equalToSuperview().inset(12)
-            $0.width.height.equalTo(28)
+            //$0.width.height.equalTo(28)
         }
         
         bottomBorder.snp.makeConstraints {
