ada7c061bceff1391fb629f81e7a587dfe9d5fc6
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-01-14 00:36:18 +0900

    Add alert action (#6)
    * [feat] title과 action style을 받는 protocol 선언
    
    * [feat] alert의 action을 받았을때, alertcontroller 생성할 수 있는 함수 구현

--- LikeBlog/LikeBlog/View/MainViewController.swift
+++ LikeBlog/LikeBlog/View/MainViewController.swift
@@ -60,3 +60,65 @@ class MainViewController: UIViewController {
     }
 
 }
+
+// Alert
+extension MainViewController {
+    typealias Alert = (title: String?, message: String?, actions : [AlertAction], style : UIAlertAction.Style)
+    
+    enum AlertAction : AlertActionConvertible {
+        //액션들을 선언
+        case title,datetime,cancel
+        case confirm
+        
+        var title: String {
+            switch self {
+            case .title :
+                return "Title"
+            case .datetime :
+                return "Datetime"
+            case .cancel :
+                return "취소"
+            case . confirm :
+                return "확인"
+            }
+        }
+        
+        var style: UIAlertAction.Style {
+            switch self {
+            case .title, .datetime :
+                return .default
+            case .cancel, .confirm :
+                return .cancel
+            }
+        }
+    }
+    // Observable : 값을 방출
+    //MARK: alert의 action을 받았을때, alertcontroller 생성할 수 있는 함수
+    func presentAlertController<Action : AlertActionConvertible>(_ alertController : UIAlertController, actions: [Action]) -> Signal<Action> {
+        if actions.isEmpty {return .empty()}
+        return Observable
+            .create { [weak self] observer in
+                //캡쳐: 자신의 블록 외부에 있는 값을 참조하는 것
+                //클로저 내에서 외부 프로퍼티를 캡처할때, 현재 instance가 heap에 존재하면 객체를 Strong으로 참조하고, instance가 해제 되었으면 nil을 반환하여 탈출
+                /*
+                 - 이스케이핑 클로저 안에서 지연할당의 가능성이 있는 경우 (API 비동기 데이터 처리, 타이머 등)
+                 - 이스케이핑 클로저가 아닌 일반 클로저에서는 Scope안에서 즉시 실행되므로 Strong Reference Cycle을 유발하지 않으므로, weak self를 사용할 필요가 없다.
+                 */
+                guard let self = self else {return Disposables.create() }
+                for action in actions {
+                    alertController.addAction(
+                        UIAlertAction(title: action.title, style: action.style, handler: {_ in
+                            observer.onNext(action)
+                            observer.onCompleted()
+                        })
+                    )
+                }
+                self.present(alertController, animated: true, completion: nil)
+                // observable 생성되면 alertController dismiss
+                return Disposables.create {
+                    alertController.dismiss(animated: true, completion: nil)
+                }
+            }
+            .asSignal(onErrorSignalWith: .empty())
+    }
+}
