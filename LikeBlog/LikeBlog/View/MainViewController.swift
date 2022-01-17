e8721d80c41cbbe2178e7d6368fc73d1c083a793
iammiori <immioriii@gmail.com>
2022-01-17 20:00:05 +0900

    [feat] 네트워크 한 결과 셀데이터로 변환후 bind

--- LikeBlog/LikeBlog/View/MainViewController.swift
+++ LikeBlog/LikeBlog/View/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Foundation
 import SnapKit
 import RxSwift
 import RxCocoa
@@ -34,12 +35,91 @@ class MainViewController: UIViewController {
     
     // for Rx
     private func bind() {
+        // searchBar 에서 shouldLoadResult Observable에서 이벤트 나오면 작동
+        let blogResult = searchBar.shouldLoadResult
+            .flatMapLatest { query in
+                SearchBlogNetwork().searchNetwork(query: query)
+            }
+            // stream 새로 만드는게 아니라 공유할거라서
+            .share()
+        
+        // 다음 블로그 값만 넣어주기
+        let blogValue = blogResult
+            .compactMap { data -> DaumBlog? in
+                //enums can have associated values
+                guard case .success(let value) = data else {
+                    return nil
+                }
+                return value
+            }
+        // 에러값
+        let blogError = blogResult
+            .compactMap { data -> String? in
+                guard case .failure(let error) = data else {
+                    return nil
+                }
+                return error.localizedDescription
+            }
+        
+        // 네트워크를 통해 가져온 값 -> 셀데이터로 변환
+        let cellData = blogValue
+            .map { blog -> [BlogListCellData] in
+                return blog.documents
+                    .map { doc in
+                        let thumbnailURL = URL(string: doc.thumbnail ?? "")
+                        return BlogListCellData(thumbnailURL: thumbnailURL,
+                                                name:  doc.blogname,
+                                                title: doc.title,
+                                                datetime: doc.datetime)
+                    }
+            }
+        
+        // filter btn 누르면 나오는 alert sheet 선택했을때 type
+        let sortedType = alertActionTapped
+            .filter {
+                switch $0 {
+                case .title, .datetime :
+                    return true
+                default :
+                    return false
+                }
+            }
+            .startWith(.title)
+        
+        
+        // MainVC -> ListView
+        // cellData _ sortedType
+        Observable
+            .combineLatest(sortedType, cellData) { type, data -> [BlogListCellData] in
+                switch type {
+                case .title :
+                    return data.sorted { $0.title ?? "" < $1.title ?? ""}
+                case .datetime :
+                    return data.sorted { $0.datetime ?? Date() > $1.datetime ?? Date() }
+                default :
+                    return data
+                }
+            }
+            .bind(to: listView.cellData)
+            .disposed(by: disposeBag)
         //listView.headerView.sortBtnTapped 이벤트를 -> alert으로
         let alertSheetForSorting = listView.headerView.sortBtnTapped
             .map { _ -> Alert in
                 return (title: nil, message: nil, actions : [.title, .datetime, .cancel], style : .actionSheet)
             }
-        alertSheetForSorting.asSignal(onErrorSignalWith: .empty())
+        let alertForMessage = blogError
+            .map { msg -> Alert in
+                return (
+                    title : "⚠️",
+                    message : msg,
+                    actions : [.confirm],
+                    style : .alert
+                )
+            }
+        
+        Observable
+            .merge(alertForMessage, alertSheetForSorting)
+            .asSignal(onErrorSignalWith: .empty())
         //. flatMapLatest는 새로운 스트림을 만들고 동작을 수행하는 도중, 새로운 아이템이 방출되게 된다면, 이전 스트림을 dispose 하고 새롭게 들어오게 되는 아이템에 대해 스트림을 생성하여 동작
             .flatMapLatest { alert -> Signal<AlertAction> in
                 let alertController = UIAlertController(title: alert.title, message: alert.message, preferredStyle: alert.style)
