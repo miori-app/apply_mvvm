15b3567596ddae2eec0b31b9c48983ce6073cfd1
iammiori <immioriii@gmail.com>
2022-01-20 22:58:10 +0900

    [refactor] 뷰모델에 맞게 리팩토링 위해 코드 주석처리

--- LikeBlog/LikeBlog/View/BlogListView.swift
+++ LikeBlog/LikeBlog/View/BlogListView.swift
@@ -12,15 +12,16 @@ import UIKit
 class BlogListView : UITableView {
     let disposeBag = DisposeBag()
     let headerView = FilterView(frame: CGRect(origin: .zero, size: CGSize(width: Constant.deviceWidth, height: 50)))
-    
-    // MainViewController -> BlogListView
-    // 받아올 이벤트
-    let cellData = PublishSubject<[BlogListCellData]>()
+
+    // MVVM 이동 (viewModel 로)
+//    // MainViewController -> BlogListView
+//    // 받아올 이벤트
+//    let cellData = PublishSubject<[BlogListCellData]>()
     
     override init(frame: CGRect, style: UITableView.Style) {
         super.init(frame: frame, style: style)
         
-        bind()
+        // bind()
         attribute()
     }
     
@@ -28,12 +29,12 @@ class BlogListView : UITableView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func bind() {
+    private func bind(_ viewModel : BlogListViewModel) {
         //셀 데이터를 받아서, Observable 어떻게 처리할지
         // mainViewController가 celldata 잘 전달해주면, 이걸 어떻게 표현할지
         // tableview delegate : cellForRowAt
-        cellData
-            .asDriver(onErrorJustReturn: [])
+        viewModel.cellData
+            //.asDriver(onErrorJustReturn: [])
             .drive(self.rx.items) { tv, row, data in
                 let index = IndexPath(row: row, section: 0)
                 let cell = tv.dequeueReusableCell(withIdentifier: "BlogListCell", for: index) as! BlogListCell
@@ -47,7 +48,7 @@ class BlogListView : UITableView {
         self.backgroundColor = .white
         self.register(BlogListCell.self, forCellReuseIdentifier: BlogListCell.registerID)
         self.separatorStyle = .singleLine
-        self.rowHeight = 50
+        self.rowHeight = 96 
         self.tableHeaderView = headerView
     }
 }
