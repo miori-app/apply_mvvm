44b7a774af152a3da7315d5ae16d094ebf56731f
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-01-25 23:58:22 +0900

    Set kakao map env (#18)
    * [feat] 카카오맵 api 사용위해 프레임워크 추가
    
    * [feat] 스위프트와 Objective-C 코드를 같이 쓰기 위해서 브릿지헤더추가
    
    * [feat] kakao map sdk 설정 (브릿지헤더, object c arc - no)

---
+++ NearByApp/NearByApp/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  NearByApp
+//
+//  Created by miori Lee on 2022/01/25.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import <DaumMap/MTMapView.h>
+
+#endif /* BridgingHeader_h */
