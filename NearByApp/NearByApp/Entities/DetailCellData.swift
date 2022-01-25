4622b21d9e55554e52b166f48da3798610332daf
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-01-26 01:02:23 +0900

    Make entity (#19)
    * [feat] api get 했을때 response entity 작성
    
    * [feat] 셀에 뿌려질 데이터 entity 설정

---
+++ NearByApp/NearByApp/Entities/DetailCellData.swift
@@ -0,0 +1,16 @@
+//
+//  DetailCellData.swift
+//  NearByApp
+//
+//  Created by miori Lee on 2022/01/26.
+//
+
+import Foundation
+
+struct DetailCellData {
+    let placeName : String
+    let addressName : String
+    let roadAddressName : String
+    // 셀 탭했을때, 지도에 띄우기위해
+    let point : MTMapPoint
+}
