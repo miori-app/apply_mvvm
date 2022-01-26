c7f09f009536e04329ba67c17e0bcb15c494841b
iammiori <immioriii@gmail.com>
2022-01-26 23:28:18 +0900

    [feat] 메인이될 뷰컨트롤러 생성

---
+++ NearByApp/NearByApp/ViewController/LocationInfoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LocationInfoViewController.swift
+//  NearByApp
+//
+//  Created by miori Lee on 2022/01/26.
+//
+
+import UIKit
+
+class LocationInfoViewController : UIViewController {
+    override func viewDidLoad() {
+    }
+}
