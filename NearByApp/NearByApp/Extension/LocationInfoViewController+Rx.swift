25c64c79f0fd2bc6a1298a542d29ea009d3224d0
iammiori <immioriii@gmail.com>
2022-01-31 04:34:30 +0900

    [feat] 핑선택시 해당하는 idx값으로 테이블뷰 이동시키는 extension + 지도에 핑찍는 extension 추가

---
+++ NearByApp/NearByApp/Extension/LocationInfoViewController+Rx.swift
@@ -0,0 +1,40 @@
+//
+//  LocationInfoViewController+Rx.swift
+//  NearByApp
+//
+//  Created by miori Lee on 2022/01/31.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+extension Reactive where Base : LocationInfoViewController {
+    
+    var showSelectedLocation : Binder<Int> {
+        return Binder(base) {base, row in
+            let indexPath = IndexPath(row: row, section: 0)
+            base.detailList.selectRow(at: indexPath, animated: true, scrollPosition: .top)
+        }
+    }
+    
+    var addPOIItemRx : Binder<[MTMapPoint]> {
+        return Binder(base) { base, points in
+            let items = points
+                .enumerated()
+                .map { offset, point -> MTMapPOIItem in
+                    let mapPOIItem = MTMapPOIItem()
+                    //지도상 좌표
+                    mapPOIItem.mapPoint = point
+                    mapPOIItem.markerType = .bluePin
+                    mapPOIItem.showAnimationType = .springFromGround
+                    mapPOIItem.tag = offset
+                    
+                    return mapPOIItem
+                }
+            base.mapView.removeAllPOIItems()
+            base.mapView.addPOIItems(items)
+        }
+    }
+}
+
