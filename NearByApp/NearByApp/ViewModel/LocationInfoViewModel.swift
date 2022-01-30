7ca10d725de427e6dddb6913b74f7169dfc62dd6
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-01-31 01:36:19 +0900

    [feat] 리스트에서 선택된 데이터중 x,y를 string-> MTMapPoint로 변경, 핑 선택시 태그 전달 (#31)

--- NearByApp/NearByApp/ViewModel/LocationInfoViewModel.swift
+++ NearByApp/NearByApp/ViewModel/LocationInfoViewModel.swift
@@ -12,6 +12,9 @@ import RxSwift
 struct LocationInfoViewModel {
     let disposeBag = DisposeBag()
     
+    //subView viewModel
+    let detailListBackgroundViewModel = DetailListBackgroundViewModel()
+    
     // viewModel -> view
     
     //Driver는 BehaviorRelay, Signal은 PublishRelay와 많은 공통점
@@ -20,15 +23,38 @@ struct LocationInfoViewModel {
     let setMapCenter : Signal<MTMapPoint>
     let errorMessage : Signal<String>
     
+    //tableView에 뿌리기 위해
+    //let detailListCellData : Driver<[DetailCellData]>
+    //핑 선택시 테이블뷰 셀 인덱스로 이동위해
+    let scrollToSelectedLocation : Signal<Int>
+    
     // view -> viewModel
     let currentLocation = PublishRelay<MTMapPoint>()
     let mapCenterPoint = PublishRelay<MTMapPoint>()
     let selectPOIItem = PublishRelay<MTMapPOIItem>()
     let mapViewError =  PublishRelay<String>()
     let currentLocationBtnTapped = PublishRelay<Void>()
+    // 리스트 선택시 row값
+    let detailListItemSelected = PublishRelay<Int>()
+    //api데이터용
+    let documetData = PublishSubject<[DetailLocData?]>()
     
     init() {
         //MARK: 지도의 중심점 선택
+        /*
+         api통신을 통해 json데이터를 받게되면,그중 선택된 리스트 값의 entity 중 x,y 값 string  -> 위도 경도 더블로 변경
+         */
+        let selectDetailListItem = detailListItemSelected
+            .withLatestFrom(documetData) { $1[$0] }
+            .map { datas -> MTMapPoint in
+                guard let data = datas,
+                      let lon = Double(data.x),
+                      let lat = Double(data.y) else {
+                          return MTMapPoint()
+                      }
+                let geoCoord = MTMapPointGeo(latitude: lat, longitude: lon)
+                return MTMapPoint(geoCoord: geoCoord)
+            }
         let moveToCurrentLocation = currentLocationBtnTapped
             // 현재위치 한번이라도 받은 이후에
             .withLatestFrom(currentLocation)
@@ -37,7 +63,10 @@ struct LocationInfoViewModel {
             .merge(
                 //주로 단 1개의 아이템만 내보내는 것을 보장하기 위해 take(1) 형태로 사용
                 currentLocation.take(1),
-                moveToCurrentLocation
+                //새로운 위치 버튼을 누르거나
+                moveToCurrentLocation,
+                //리스트에서 선택한 경우
+                selectDetailListItem
             )
         
         setMapCenter = currentMapCenter
@@ -45,5 +74,12 @@ struct LocationInfoViewModel {
         
         errorMessage = mapViewError.asObservable()
             .asSignal(onErrorJustReturn: "잠시 후 다시 시도해주세요")
+        
+        //detailListCellData
+        
+        //selectPOIItem : 핑을 선택했을때 발생하는 이벤트
+        scrollToSelectedLocation = selectPOIItem
+            .map { $0.tag }
+            .asSignal(onErrorJustReturn: 0)
     }
 }
