1afea6bc7a0f64612c9068d392b4d7fd5d1ede65
iammiori <immioriii@gmail.com>
2022-02-01 14:29:27 +0900

    [feat] 네트워크 모델 구현

---
+++ NearByApp/NearByApp/Model/LocationInfoModel.swift
@@ -0,0 +1,37 @@
+//
+//  LocationInfoModel.swift
+//  NearByApp
+//
+//  Created by miori Lee on 2022/02/01.
+//
+
+import Foundation
+import RxSwift
+
+struct LocationInfoModel {
+    let localNetwork : LocalNetwork
+    
+    init(localNetwork : LocalNetwork = LocalNetwork()) {
+        self.localNetwork = localNetwork
+    }
+    
+    func getLocation(by mapPoint: MTMapPoint) -> Single<Result<LocationResponse,URLError>> {
+        return localNetwork.getLocation(by: mapPoint)
+    }
+    
+    func documentsToCellData(_ data: [DetailLocData]) -> [DetailCellData] {
+        return data.map {
+            let addressName = $0.roadAddressName.isEmpty ? $0.addressName : $0.roadAddressName
+            let point = docToMapPoint($0)
+            return DetailCellData(placeName: $0.placeName, addressName: addressName, distance: $0.distance, point: point)
+        }
+    }
+    
+    func docToMapPoint(_ doc : DetailLocData) -> MTMapPoint {
+        let latitude = Double(doc.x) ?? .zero
+        let longitude = Double(doc.y) ?? .zero
+        return MTMapPoint(geoCoord: MTMapPointGeo(latitude: latitude, longitude: longitude))
+    }
+    
+    
+}
