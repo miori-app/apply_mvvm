d806095b433d43381178bbd26be254bd91203693
Mi Yeon Lee <immioriii@gmail.com>
2022-09-13 22:25:18 +0900

    feat: 화면 너비, 높이 constant 추가

---
+++ NearByApp/NearByApp/Configuration/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  NearByApp
+//
+//  Created by miori Lee on 2022/02/03.
+//
+
+import Foundation
+import UIKit
+
+struct Constant {
+    static let deviceWidth = UIScreen.main.bounds.width
+    static let deviceHeight = UIScreen.main.bounds.height
+}
