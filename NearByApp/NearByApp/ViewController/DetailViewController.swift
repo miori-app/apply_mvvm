2cd79009f5adf9e00c579691aa2e16abe7e7d85d
Mi Yeon Lee <immioriii@gmail.com>
2022-09-18 23:54:30 +0900

    feat:  상세 설명을 볼 수 있는 detailVC 생성

---
+++ NearByApp/NearByApp/ViewController/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  NearByApp
+//
+//  Created by miori Lee on 2022/05/11.
+//
+
+import Foundation
+
+class DetailViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("hello")
+    }
+}
